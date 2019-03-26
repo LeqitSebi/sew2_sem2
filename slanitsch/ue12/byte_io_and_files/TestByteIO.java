17ab1d0b6a0b04a9042fff5e7c1760afdcc49230
Sebastian Slanitsch <7225@htl.rennweg.at>
2019-03-26 10:04:38 +0100

    schreiben von Split und Unsplit File

--- slanitsch/ue12/byte_io_and_files/TestByteIO.java
+++ slanitsch/ue12/byte_io_and_files/TestByteIO.java
@@ -13,8 +13,11 @@ public class TestByteIO {
     public static void main(String[] args) throws IOException {
 //        copyFile(Paths.get("resources/gnu_pingu.jpg"), Paths.get("resources/mein_bild.jpg"));
 //        invertFile(Paths.get("resources/Was_stellt_das_verschluesselte_Bild_dar.gif"), Paths.get("resources/mein_bild_test.jpg"));
-        encryptFile(Paths.get("resources/gnu_pingu.jpg"), Paths.get("resources/mein_bild.jpg"), 12);
-        decryptFile(Paths.get("resources/mein_bild.jpg"), Paths.get("resources/mein_bild_test.jpg"), 12);
+//        encryptFile(Paths.get("resources/gnu_pingu.jpg"), Paths.get("resources/mein_bild.jpg"), 12);
+//        decryptFile(Paths.get("resources/mein_bild.jpg"), Paths.get("resources/mein_bild_test.jpg"), 12);
+//        FileSplit(Paths.get("resources/gnu_pingu.jpg"), 15000);
+        FileUnsplit(Paths.get("resources/gnu_pingu.jpg"));
+
     }
 
     public static void copyFile(Path srcFile, Path destFile) throws IOException {
@@ -68,4 +71,36 @@ public class TestByteIO {
     public static void decryptFile(Path srcFile, Path destFile, int key) throws IOException {
         encryptFile(srcFile, destFile, key);
     }
+
+    public static void FileSplit(Path file, long maxBytes) throws IOException {
+        InputStream in = Files.newInputStream(file);
+        float helper = (float) Files.size(file) / maxBytes;
+        byte[] buffer = new byte[(int) maxBytes];
+        int bytesRead;
+        double filecount = Math.ceil(helper);
+        int i = 0;
+        while ((bytesRead = in.read(buffer, 0, buffer.length)) > 0) {
+            OutputStream out = Files.newOutputStream(Paths.get("resources/splitFile0000000" + i + ".jpg"));
+            out.write(buffer, 0, bytesRead);
+            i++;
+        }
+    }
+
+    public static void FileUnsplit(Path file) throws IOException {
+        OutputStream out = Files.newOutputStream(Paths.get("resources/Unsplitted.jpg"));
+        byte[] buffer = new byte[2048];
+        int bytesRead;
+        for (int i = 0; true; i++) {
+            if (Files.exists(Paths.get("resources/splitFile0000000" + i + ".jpg"))) {
+                InputStream in = Files.newInputStream(Paths.get("resources/splitFile0000000" + i + ".jpg"));
+                while ((bytesRead = in.read(buffer, 0, buffer.length)) > 0) {
+                    out.write(buffer, 0, bytesRead);
+                }
+            } else {
+                break;
+            }
+        }
+    }
 }
+
+
