799ac8aac46fb85962a06cf52d06d4cd0eade1bb
Sebastian Slanitsch <7225@htl.rennweg.at>
2019-04-24 20:10:17 +0200

    programmieren von calcGewinne und printCalcGewinne. jetzt ist die ue13 fertig :)

--- slanitsch/ue13/collections/LottoTipp.java
+++ slanitsch/ue13/collections/LottoTipp.java
@@ -1,5 +1,6 @@
 package slanitsch.ue13.collections;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
@@ -11,13 +12,15 @@ public class LottoTipp {
     public static void main(String[] args) {
         LottoTipp a = new LottoTipp();
         LottoTipp b = new LottoTipp();
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println("a.countEqualNumbers(b): " + a.countEqualNumbers(b));
-        System.out.println("a.countEqualNumbers(a): 6 | " + a.countEqualNumbers(a));
-        System.out.println("a.equals(b): " + a.equals(b));
-        System.out.println("a.equals(a): true | " + a.equals(a));
-        System.out.println(createLottoTipps(6));
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println("a.countEqualNumbers(b): " + a.countEqualNumbers(b));
+//        System.out.println("a.countEqualNumbers(a): 6 | " + a.countEqualNumbers(a));
+//        System.out.println("a.equals(b): " + a.equals(b));
+//        System.out.println("a.equals(a): true | " + a.equals(a));
+//        System.out.println(createLottoTipps(6));
+        printCalcGewinne(calcGewinne(2));
+
     }
 
     public LottoTipp() {
@@ -80,4 +83,27 @@ public class LottoTipp {
         return lottoTipps;
     }
 
+    public static int[] calcGewinne(int seconds) {
+        LottoTipp rightTipp = new LottoTipp();
+        int right_guesses;
+        int[] output = new int[7];
+        long endTime = System.currentTimeMillis() + (seconds*1000);
+        while (System.currentTimeMillis() <= endTime){
+                LottoTipp guess = new LottoTipp();
+                right_guesses = guess.countEqualNumbers(rightTipp);
+                output[right_guesses]++;
+        }
+        return output;
+    }
+
+    public static void printCalcGewinne(int[]gewinne){
+        System.out.println("Ein 0er, 1er, etc. kam insgesamt vor:");
+        System.out.println("0er: " + gewinne[0]);
+        System.out.println("1er: " + gewinne[1]);
+        System.out.println("2er: " + gewinne[2]);
+        System.out.println("3er: " + gewinne[3]);
+        System.out.println("4er: " + gewinne[4]);
+        System.out.println("5er: " + gewinne[5]);
+        System.out.println("6er: " + gewinne[6]);
+    }
 }
