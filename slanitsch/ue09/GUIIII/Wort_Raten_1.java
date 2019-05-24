33b91fdf81a16d8276cd2a75af8f4379a3a3c581
slanitsch <7225@htl.rennweg.at>
2019-05-24 07:53:49 +0200

    erste funktionierende Version des Spiels anzeige der benötigten Tipps

--- slanitsch/ue09/GUIIII/Wort_Raten_1.java
+++ slanitsch/ue09/GUIIII/Wort_Raten_1.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 public class Wort_Raten_1 extends Application {
     public static int get_to_solve=0;
-    public static String[] getWord() throws IOException {
+    public static String getWord() throws IOException {
         try (
                 BufferedReader in = Files.newBufferedReader(Paths.get("resources/words.txt"), Charset.forName("UTF-8"));
         ) {
@@ -29,11 +29,7 @@ public class Wort_Raten_1 extends Application {
             for (int j = 0; j < rnd; j++) {
                 line = in.readLine();
             }
-            String[] output = new String[line.length()];
-            for (int i = 0; i < line.length(); i++) {
-                output[i] = "" + line.charAt(i);
-            }
-            return output;
+            return line;
         }
     }
 
@@ -43,7 +39,11 @@ public class Wort_Raten_1 extends Application {
 
     public void start(Stage stage) throws IOException {
 
-        String[] Wort = getWord();
+        String Worts = getWord();
+        String[] Wort = new String[Worts.length()];
+        for (int i = 0; i < Worts.length(); i++) {
+            Wort[i] = "" + Worts.charAt(i);
+        }
         GridPane gridPane = new GridPane();
         Scene scene = new Scene(gridPane, 550, 210); // w, h
         gridPane.setHgap(20);
@@ -52,17 +52,35 @@ public class Wort_Raten_1 extends Application {
         stage.setScene(scene);
         stage.setTitle("Wörter erraten");
         stage.setScene(scene);
+        Button submit = new Button("prüfen");
         EditableButton[] viele = new EditableButton[Wort.length];
         for (int i = 0; i < Wort.length; i++) {
             viele[i]=new EditableButton(Wort[i]);    
         }
         TextField z1 = new TextField();
         z1.setPrefColumnCount(10);
+        Label tries = new Label("0");
         stage.show();
         for (int i = 0; i < viele.length; i++) {
             gridPane.add(viele[i],i, 0);
         }
         gridPane.add(z1,0,2);
+        gridPane.add(submit, 0,3);
+        gridPane.add(tries, 0, 4);
+
+        submit.setOnAction(event -> {
+            try {
+                String versuch = z1.getText();
+                if(versuch.equals(Worts)){
+                    z1.setText("Richtig!!!");
+                }else{
+                    z1.setText("Falsch!!!");
+                }
+                tries.setText("Du hast " + get_to_solve + " Tipps gebraucht");
+            }catch (NumberFormatException e) {
+                z1.setText(e.getMessage());
+            }
+        });
     }
 
     public static void main(String[] args) throws IOException {
