ab1812c7a6e0e2658b12ab110179166f3e212302
slanitsch <7225@htl.rennweg.at>
2019-05-29 11:19:19 +0200

    beginn des spiels hangman überlegung des layouts

--- slanitsch/ue09/GUIIII/hangman.java
+++ slanitsch/ue09/GUIIII/hangman.java
@@ -1,5 +1,53 @@
 package slanitsch.ue09.GUIIII;
 
-public class hangman {
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class hangman extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        String Worts = Wort_Raten_1.getWord();
+        String[] Wort = new String[Worts.length()];
+        for (int i = 0; i < Worts.length(); i++) {
+            Wort[i] = "" + Worts.charAt(i);
+        }
+
+        Button[] viele = new Button[Worts.length()];
+        for (int i = 0; i < Wort.length; i++) {
+            viele[i]=new Button("?");
+        }
+
+
+        stage.setTitle("HBox Sample");
+
+        HBox hbox = new HBox();
+        Scene scene = new Scene(hbox, 600, 100);
+        stage.setScene(scene);
+
+        hbox.setPadding(new Insets(10)); 	// Set all sides to 10
+        hbox.setSpacing(15);			// Gap between nodes
+        hbox.setAlignment(Pos.TOP_CENTER);
+        for (int i = 0; i < Worts.length(); i++) {
+            hbox.getChildren().add(viele[i]);
+        }
+        TextField tf = new TextField();
+
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
